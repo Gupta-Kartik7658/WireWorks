385087894c9ef9fa13e5f1b9d19cbc151c97fae2
Kavit-Chugh <kavitchugh@gmail.com>
2025-03-25 13:52:36 +0530

    add components

--- pyqt_files/circuit_designer.py
+++ pyqt_files/circuit_designer.py
@@ -1,8 +1,9 @@
+import os
 from PyQt5.QtWidgets import (
-    QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsRectItem, QVBoxLayout,
-    QWidget, QToolBar, QAction, QGraphicsPixmapItem
+    QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, 
+    QVBoxLayout, QWidget, QToolBar, QAction
 )
-from PyQt5.QtGui import QPen, QColor, QPainter, QIcon, QPixmap
+from PyQt5.QtGui import QPixmap, QIcon, QColor, QPainter
 from PyQt5.QtCore import Qt
 
 
@@ -12,15 +13,13 @@ class diagramWorkspace(QGraphicsScene):
         self.grid_size = grid_size
 
     def drawBackground(self, painter, rect):
-        
         painter.fillRect(rect, QColor(240, 240, 240))  
+        pen = QPainter()
+        pen.setPen(QColor(200, 200, 200))
+        painter.setPen(pen)
 
         left = int(rect.left()) - (int(rect.left()) % self.grid_size)
         top = int(rect.top()) - (int(rect.top()) % self.grid_size)
-
-
-        pen = QPen(QColor(200, 200, 200))
-        painter.setPen(pen)
 
         for x in range(left, int(rect.right()), self.grid_size):
             painter.drawLine(x, int(rect.top()), x, int(rect.bottom()))
@@ -28,12 +27,10 @@ class diagramWorkspace(QGraphicsScene):
             painter.drawLine(int(rect.left()), y, int(rect.right()), y)
 
 
-
 class DraggableComponent(QGraphicsPixmapItem):
     def __init__(self, pixmap):
         super().__init__(pixmap)
         self.setFlags(QGraphicsPixmapItem.ItemIsMovable | QGraphicsPixmapItem.ItemIsSelectable)
-
 
 
 class CircuitDesigner(QMainWindow):
@@ -44,56 +41,73 @@ class CircuitDesigner(QMainWindow):
         self.scene = diagramWorkspace(20)
         self.view = QGraphicsView(self.scene)
         self.view.setRenderHint(QPainter.Antialiasing)
+
         central_widget = QWidget()
         layout = QVBoxLayout()
         layout.addWidget(self.view)
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
-        
         self.create_toolbar()
 
     def create_toolbar(self):
-        toolbar = QToolBar("Componetn Library")
+        toolbar = QToolBar("Component Library")
         self.addToolBar(toolbar)
-        resistor_action = QAction(QIcon("resistor.svg"), "Add Resistor", self)
-        resistor_action.triggered.connect(lambda: self.add_component("Resistor"))
-        toolbar.addAction(resistor_action)
 
-        
-        led_action = QAction(QIcon("led.svg"), "Add LED", self)
-        led_action.triggered.connect(lambda: self.add_component("LED"))
-        toolbar.addAction(led_action)
+        components = {
+            "Resistor": "resistor.svg",
+            "LED": "led.svg",
+            "RGB LED": "rgb_led.svg",
+            "Transistor": "transistor.svg",
+            "Potentiometer": "potentiometer.svg",
+            "DHT11": "dht11.svg",
+            "ESP32": "esp32.svg",
+            "Microcontroller": "microcontroller.svg",
+            "Arduino Uno": "arduino_uno.svg",
+            "Arduino Mega": "arduino_mega.svg",
+            "Arduino Nano": "arduino_nano.svg"
+        }
 
-        
-        microcontroller_action = QAction(QIcon("C:\\Users\\hp\\Desktop\\WireWorks\\microcontroller.svg"), "Add Microcontroller", self)
-        microcontroller_action.triggered.connect(lambda: self.add_component("Microcontroller"))
-        toolbar.addAction(microcontroller_action)
+        for name, icon_path in components.items():
+            if not os.path.exists(icon_path):
+                print(f"Warning: {icon_path} not found!")  
 
-        
-        arduino_action = QAction(QIcon("C:\\Users\\hp\\Desktop\\WireWorks\\arduino_uno.svg"), "Add Arduino Uno", self)
-        arduino_action.triggered.connect(lambda: self.add_component("Arduino Uno"))
-        toolbar.addAction(arduino_action)
-
+            action = QAction(QIcon(icon_path), f"Add {name}", self)
+            action.triggered.connect(lambda checked, t=name: self.add_component(t))
+            toolbar.addAction(action)
 
     def add_component(self, component_type):
-        
-        if component_type == "Resistor":
-            component = DraggableComponent(self.create_pixmap(60, 20, QColor(200, 50, 50)))  
-        elif component_type == "LED":
-            component = DraggableComponent(self.create_pixmap(20, 20, QColor(50, 200, 50)))  
-        elif component_type == "Microcontroller":
-            component = DraggableComponent(self.create_pixmap(80, 40, QColor(50, 50, 200)))  
-        elif component_type == "Arduino Uno":
-            pixmap = QPixmap("C:\\Users\\hp\\Desktop\\WireWorks\\arduino_uno.svg")
-            component = DraggableComponent(pixmap)
-        else:
-            return
-        center_x = self.view.mapToScene(self.view.viewport().rect().center()).x()
-        center_y = self.view.mapToScene(self.view.viewport().rect().center()).y()
-        component.setPos(center_x - component.pixmap().width() / 2, center_y - component.pixmap().height() / 2)
-        self.scene.addItem(component)
+        component = None
 
+        if component_type == "Resistor":
+            component = DraggableComponent(self.create_pixmap(60, 20, QColor(200, 50, 50)))
+        elif component_type == "LED":
+            component = DraggableComponent(self.create_pixmap(20, 20, QColor(50, 200, 50)))
+        elif component_type == "RGB LED":
+            component = DraggableComponent(self.create_pixmap(25, 25, QColor(50, 50, 200)))  
+        elif component_type == "Transistor":
+            component = DraggableComponent(self.create_pixmap(30, 40, QColor(100, 100, 100)))  
+        elif component_type == "Potentiometer":
+            component = DraggableComponent(self.create_pixmap(40, 40, QColor(150, 100, 50)))  
+        elif component_type == "DHT11":
+            component = DraggableComponent(self.create_pixmap(40, 50, QColor(0, 100, 255)))  
+        elif component_type == "ESP32":
+            component = DraggableComponent(self.create_pixmap(60, 40, QColor(255, 50, 0)))  
+        elif component_type == "Microcontroller":
+            component = DraggableComponent(self.create_pixmap(80, 40, QColor(50, 50, 200)))
+        elif component_type in ["Arduino Uno", "Arduino Mega", "Arduino Nano"]:
+            image_path = f"{component_type.lower().replace(' ', '_')}.svg"
+            if os.path.exists(image_path):
+                component = DraggableComponent(QPixmap(image_path))
+            else:
+                print(f"Error: {image_path} not found!")  
+                return
+
+        if component:
+            center_x = self.view.mapToScene(self.view.viewport().rect().center()).x()
+            center_y = self.view.mapToScene(self.view.viewport().rect().center()).y()
+            component.setPos(center_x - component.pixmap().width() / 2, center_y - component.pixmap().height() / 2)
+            self.scene.addItem(component)
 
     def create_pixmap(self, width, height, color):
         pixmap = QPixmap(width, height)
@@ -106,3 +120,6 @@ if __name__ == "__main__":
     window = CircuitDesigner()
     window.show()
     app.exec_()
+
+
+
